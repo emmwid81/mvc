1a98665829da4ea8b68aee4b27e98c959f143e4e
Emma <emmawidengrd@yahoo.se>
2025-04-09 11:46:42 +0200

    Working on kmom01

--- src/Controller/LuckyControllerTwig.php
+++ src/Controller/LuckyControllerTwig.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyControllerTwig extends AbstractController
 {
-    #[Route("/lucky/number/twig", name: "lucky_number")]
+    #[Route("/lucky/twig", name: "lucky")]
     public function number(): Response
     {
         $number = random_int(0, 100);
@@ -17,17 +17,6 @@ class LuckyControllerTwig extends AbstractController
             'number' => $number
         ];
 
-        return $this->render('lucky_number.html.twig', $data);
-    }
-        #[Route("/home", name: "home")]
-    public function home(): Response
-    {
-        return $this->render('home.html.twig');
-    }
-
-    #[Route("/about", name: "about")]
-    public function about(): Response
-    {
-        return $this->render('about.html.twig');
+        return $this->render('lucky.html.twig', $data);
     }
 }
